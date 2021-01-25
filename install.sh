8a09e4081754d51b6aed9fb5e68afa50fcc6ff3f
Josh Long <josh@joshlong.com>
2021-01-24 23:52:27 -0800

    install

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@
 
 helm repo add this-week-in-charts https://this-week-in-charts.storage.googleapis.com 
 helm repo add stable https://charts.helm.sh/stable
+helm upgrade
 
-
-INGEST_FEED_ENCODED_MAPPINGS=$( base64 feed-mappings.json   )
-INGEST_TWITTER_ENCODED_MAPPINGS=$( base64 twitter-mappings.json     )
+# INGEST_FEED_ENCODED_MAPPINGS=$( base64 feed-mappings.json   )
+# INGEST_TWITTER_ENCODED_MAPPINGS=$( base64 twitter-mappings.json     )
 NS=$TWI_NS
 CHART_NAME=twi-${NS}-helm-chart 
 
