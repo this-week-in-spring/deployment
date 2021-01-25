4ec5ba65a28db4da47cc5d7dd40aa05d57a134cd
Josh Long <josh@joshlong.com>
2021-01-25 00:05:17 -0800

    up

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash 
 
-
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash 
+ls -la
+which helm 
 
 helm repo add this-week-in-charts https://this-week-in-charts.storage.googleapis.com 
 helm repo add stable https://charts.helm.sh/stable
