1ecaac16c3adab22a3b0201ece7ca0458b2bfe51
Josh Long <josh@joshlong.com>
2023-06-06 16:14:50 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash 
 
 NS=$TWI_NS
-CHART_NAME=twi-${NS}-helm-chart
-INGEST_FEED_ENCODED_MAPPINGS=$( cat feed-mappings.json | base64 | tr -d '\n' )
-INGEST_TWITTER_ENCODED_MAPPINGS=$( cat twitter-mappings.json | base64 | tr -d '\n' )
-
-helm repo add this-week-in-charts https://this-week-in.github.io/helm-charts/
-helm repo add stable https://charts.helm.sh/stable
-kubectl get namespaces 
+CHART_NAME=twi-${NS}-helm-chart 
 
 function create_ip(){
     IPN=$1
@@ -15,24 +9,26 @@ function create_ip(){
 }
 
 function init(){
-    create_ip ${NS}-twi-studio-ip 
+    create_ip ${NS}-twi-studio-client-ip 
+    create_ip ${NS}-twi-studio-gateway-ip 
     create_ip ${NS}-twi-bookmark-api-ip 
     kubectl get ns/$NS || kubectl create namespace ${NS} 
 }
-
 
 init 
 
 
 HELM_COMMAND="install"
-helm list --all-namespaces | grep $CHART_NAME  && HELM_COMMAND="upgrade"
-helm $HELM_COMMAND  \
- --set twi.prefix=$NS \
- --set twi.domain=$TWI_DOMAIN \
- --set twi.postgres.username=$DB_USER \
- --set twi.postgres.password=$DB_PW \
- --set twi.postgres.host=$DB_HOST \
- --set twi.postgres.schema=$DB_DB \
+helm list -n $NS | grep $CHART_NAME  && HELM_COMMAND="upgrade" 
+echo "Performing a helm ${HELM_COMMAND}..."
+
+helm  $HELM_COMMAND   --values ./values.yaml  \
+ --set twi.prefix=$NS   \
+ --set twi.domain=$TWI_DOMAIN  \
+ --set twi.postgres.username=$DB_USER  \
+ --set twi.postgres.password=$DB_PW  \
+ --set twi.postgres.host=$DB_HOST  \
+ --set twi.postgres.schema=$DB_DB  \
  --set twi.redis.host=$REDIS_HOST \
  --set twi.redis.password=$REDIS_PW \
  --set twi.redis.port=$REDIS_PORT \
@@ -44,7 +40,7 @@ helm $HELM_COMMAND  \
  --set twi.ingest.feed.mappings=$INGEST_FEED_ENCODED_MAPPINGS \
  --set twi.ingest.twitter.mappings=$INGEST_TWITTER_ENCODED_MAPPINGS \
  --namespace $NS  \
- $CHART_NAME this-week-in-charts/twi-helm-chart   
+ $CHART_NAME . 
 
 kubectl create job --from=cronjob/${NS}-twi-twitter-ingest-cronjob ${NS}-twi-twitter-ingest-cronjob-${RANDOM} -n $NS 
 kubectl create job --from=cronjob/${NS}-twi-bookmark-ingest-cronjob ${NS}-twi-bookmark-ingest-cronjob-${RANDOM} -n $NS 
